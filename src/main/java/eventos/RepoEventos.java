df41f8c00205f2ae43073747c805b4ff34b94120
vero <vero.camiz@hotmail.com>
2019-07-04 20:41:16 -0300

    pronostico

---
+++ src/main/java/eventos/RepoEventos.java
@@ -0,0 +1,27 @@
+package eventos;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+
+public class RepoEventos {
+	
+		private static List<Evento> eventos = new ArrayList<>();
+	    private static RepoEventos instance;
+	    
+	    private RepoEventos(){}
+	    
+	    public static RepoEventos getInstance(){
+	        if(instance == null){
+	            instance = new RepoEventos();
+	        }
+	        return instance;
+	    }
+	    
+	    public static void agendar(Evento evento) {
+	    	eventos.add(evento);
+	    }
+	 
+	    
+	
+}
