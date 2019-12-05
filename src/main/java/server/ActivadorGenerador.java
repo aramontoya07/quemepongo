1ef620074dd96cdd7886e63758bd2b16de30b9ff
Maximiliano Arrieta <arrieta.maxi@gmail.com>
2019-12-05 04:14:40 -0300

    rompi algo?

---
+++ src/main/java/server/ActivadorGenerador.java
@@ -0,0 +1,18 @@
+package server;
+
+import java.util.List;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import repositorios.RepositorioUsuarios;
+import usuario.Usuario;
+
+
+
+public class ActivadorGenerador implements Job{
+    public void execute(JobExecutionContext contexto){
+        List<Usuario> usuarios = RepositorioUsuarios.ObtenerUsuariosTotales();
+        usuarios.forEach(usuario -> usuario.generarSugerenciasNecesarias());
+        System.out.println("ejecute qcyo");
+    }
+}
--- src/main/java/server/GeneradorSugerenciasEventos.java
+++ src/main/java/server/GeneradorSugerenciasEventos.java
@@ -1,15 +1,37 @@
 package server;
 
-import repositorios.RepositorioUsuarios;
-import usuario.Usuario;
-
-import java.util.List;
+//import repositorios.RepositorioUsuarios;
+//import usuario.Usuario;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+import org.quartz.Scheduler;
 
 public class GeneradorSugerenciasEventos {
 
     public static void main(String[] args) {
-        List<Usuario> usuarios = RepositorioUsuarios.ObtenerUsuariosTotales();
+        Scheduler scheduler = null;
+        
+        try {
+            scheduler = new StdSchedulerFactory().getScheduler();
+            scheduler.start();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
 
-        usuarios.forEach(usuario -> usuario.generarSugerenciasNecesarias());
+        JobDetail job = JobBuilder.newJob(ActivadorGenerador.class).withIdentity("Job1" , "Grupo1")
+                .build();
+
+        Trigger trigger = TriggerBuilder.newTrigger().withIdentity("Trigger1", "Grupo1").startNow()
+            .withSchedule(SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(2)).build();
+        try {
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
     }
 }
